f1bd2389cac7bf0122fa631268b53d85bfe75a78
igroman4ik-pc <ni4000009@gmail.com>
2025-12-12 20:10:17 +0300

    Add project files.

---
+++ ViaClicker/Models/Config.cs
@@ -0,0 +1,17 @@
+﻿namespace ViaClicker
+{
+    public class ConfigClicker
+    {
+        public int IntervalMs { get; set; }
+        public MouseButton ClickButton { get; set; }
+        public ConsoleKey StartKey { get; set; }
+        public ConsoleKey PauseKey { get; set; }
+    }
+
+    public enum MouseButton
+    {
+        Left,
+        Right,
+        Middle
+    }
+}
---
+++ ViaClicker/Services/ClickerConfigService.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace ViaClicker
+{
+    public static class ClickerConfigService
+    {
+        private const string FileName = "config.json";
+
+        public static ConfigClicker Load()
+        {
+            if (!File.Exists(FileName))
+                return CreateDefault();
+            
+            var json = File.ReadAllText(FileName);
+            return JsonConvert.DeserializeObject<ConfigClicker>(json);
+        }
+
+        public static void Save(ConfigClicker config)
+        {
+            var json = JsonConvert.SerializeObject(config, Formatting.Indented);
+            File.WriteAllText(FileName, json);
+        }
+
+        private static ConfigClicker CreateDefault()
+        {
+            var cfg = new ConfigClicker
+            {
+                IntervalMs = 100,
+                ClickButton = MouseButton.Left,
+                StartKey = ConsoleKey.F6,
+                PauseKey = ConsoleKey.F7
+            };
+
+            Save(cfg);
+            return cfg;
+        }
+    }
+}
